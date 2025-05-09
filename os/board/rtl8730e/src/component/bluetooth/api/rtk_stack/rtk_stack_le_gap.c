9aa7a76cdcafaea361c8b901eb05ebd4772639a1
yeetee179 <yeetee179@gmail.com>
2025-05-09 15:14:42 +0800

    Revision: a02f9c277e067a7334b8c50ecb5904576039061d Author: frank_shen <frank_shen@realsil.com.cn> Date: 1/18/2024 3:28:21 PM Message: [amebalite][bt:ble] Add api of "rtk_bt_le_gap_set_max_mtu_size"
    + Add api of "rtk_bt_le_gap_set_max_mtu_size" and related atcmd
    
    Verification:
        AmebaLite test OK.
    
    Hook-Version: 7
    ----------
    
    Change-Id: I0dadd84a9b85f06b20c70e2e634c111092c689b2
    ----
    Modified: component/bluetooth/api/include/rtk_bt_common.h
    Modified: component/bluetooth/api/include/rtk_bt_le_gap.h
    Modified: component/bluetooth/api/rtk_bt_le_gap.c
    Modified: component/bluetooth/api/rtk_stack/rtk_stack_le_gap.c
    Modified: component/bluetooth/example/bt_atcmd/atcmd_bt_le_gap.c

--- os/board/rtl8730e/src/component/bluetooth/api/rtk_stack/rtk_stack_le_gap.c
+++ os/board/rtl8730e/src/component/bluetooth/api/rtk_stack/rtk_stack_le_gap.c
@@ -4028,6 +4028,28 @@ static uint16_t bt_stack_le_gap_get_mtu_size(void *param)
 	return 0;
 }
 
+static uint16_t bt_stack_le_gap_set_max_mtu_size(void *param)
+{
+	T_GAP_CAUSE cause;
+	T_GAP_DEV_STATE dev_state;
+	uint8_t active_conn_num = 0;
+	uint16_t mtu_size = *(uint16_t *)param;
+
+	cause = le_get_gap_param(GAP_PARAM_DEV_STATE, &dev_state);
+	if (cause) {
+		return RTK_BT_ERR_LOWER_STACK_API;
+	}
+	active_conn_num = le_get_active_link_num();
+
+	if ((GAP_CONN_STATE_CONNECTING == dev_state.gap_conn_state) || active_conn_num) {
+		return RTK_BT_ERR_STATE_INVALID;
+	}
+
+	gap_config_max_mtu_size(mtu_size);
+
+	return 0;
+}
+
 static uint16_t bt_stack_le_gap_set_channels(void *param)
 {
 	T_GAP_CAUSE cause;
