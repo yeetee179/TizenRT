ddbebf976ac7c89598efee197f7957c37cac1ea6
yeetee179 <yeetee179@gmail.com>
2025-06-25 17:30:36 +0800

    Revision: a127fdbf1b57de779dccf54d04b675dfbe0a4771 Author: frank_shen <frank_shen@realsil.com.cn> Date: 4/15/2024 9:25:50 AM Message: [amebalite][bt:ble] Add param cccd_not_save for cccd not saving when bonded
    * Add param cccd_not_save for cccd not saving when bonded.
    * Adjust the order of ble atcmd.
    
    Verification:
        AmebaLite build and test OK.
    
    Hook-Version: 7
    ----------
    
    Change-Id: Iba6186de7df9e76ad3d6a5fd14829bf696f97750
    ----
    Modified: component/bluetooth/api/include/bt_app_config.h
    Modified: component/bluetooth/api/rtk_stack/rtk_stack_api.c
    Modified: component/bluetooth/example/bt_atcmd/atcmd_bt_le_gap.c

--- os/board/rtl8730e/src/component/bluetooth/inc/bt_app_config.h
+++ os/board/rtl8730e/src/component/bluetooth/inc/bt_app_config.h
@@ -20,7 +20,7 @@ extern "C"
 #include <rtk_bt_mesh_def.h>
 #endif
 
-#define RTK_BT_LE_GAP_IRK_LEN		16
+#define RTK_BT_LE_GAP_IRK_LEN       16
 
 /**
  * @typedef rtk_bt_app_conf_t
@@ -30,12 +30,13 @@ typedef struct {
 	uint8_t  a2dp_role;                        /*!< Default A2DP profile role (Only used in BR/EDR) */
 	uint8_t  spp_role;                         /*!< Default SPP profile role (Only used in BR/EDR) */
 	uint8_t  hfp_role;                         /*!< Default HFP profile role (Only used in BR/EDR) */
+	uint8_t  hid_role;                         /*!< Default HID profile role (Only used in BR/EDR) */
 	uint32_t app_profile_support;              /*!< app profile support @ref rtk_bt_profile_t */
 	uint16_t mtu_size;                         /*!< Deafult MTU config */
-	bool     master_init_mtu_req;              /*!< Whether master auto initiate exchange MTU request when connected.
-													When this value is false, user can use @ref rtk_bt_gattc_exchange_mtu
-													to initiate exchange MTU request */
-	bool     slave_init_mtu_req;               /*!< Whether slave auto initiate exchange MTU request when connected. */
+	bool     master_init_mtu_req;              /*!< Whether local device as master will auto initiate exchange MTU request when connected.
+                                                    If this value is false, user can use @ref rtk_bt_gattc_exchange_mtu
+                                                    to initiate exchange MTU request. */
+	bool     slave_init_mtu_req;               /*!< Whether local device as slave will auto initiate exchange MTU request when connected. */
 	uint8_t  prefer_all_phy;                   /*!< Deafult PHY config: preferred all phy */
 	uint8_t  prefer_tx_phy;                    /*!< Deafult PHY config: preferred tx phy */
 	uint8_t  prefer_rx_phy;                    /*!< Deafult PHY config: preferred rx phy */
@@ -46,8 +47,12 @@ typedef struct {
 #endif
 	uint8_t  irk[RTK_BT_LE_GAP_IRK_LEN];       /*!< Local IRK data */
 	bool     user_def_service;                 /*!< Whether register gap and gatt service in app, or use builtin service in stack */
-	bool     cccd_not_check;                   /*!< Whether check if cccd is enabled before send indicate or notify. When true, cccd will not be checked;
-													when false, cccd will be checked, if bit of indicate/notify is not enabled, send indicate or notify will fail */
+	bool     cccd_not_check;                   /*!< Whether check if cccd is enabled before send indicate or notify. If true, cccd will not be checked;
+                                                    if false, cccd will be checked, if bit of indicate/notify is not enabled, send indicate or notify will fail */
+	bool     cccd_not_save;                    /*!< Whether save local server's cccd info to flash when bonded. If true, cccd will not be saved. If false,
+                                                    cccd will be saved, and when reconnect to the bonded device, @ref RTK_BT_GATTS_EVT_CCCD_IND events
+                                                    will be indicated immediately.
+                                                    By default, cccd will be saved. And this value will be ignored if @ref RTK_BLE_AUDIO_SUPPORT is enabled.*/
 #if defined(RTK_BLE_AUDIO_SUPPORT) && RTK_BLE_AUDIO_SUPPORT
 	rtk_bt_le_audio_app_conf_t le_audio_app_conf; /*!< Default configuration for le audio */
 #endif
